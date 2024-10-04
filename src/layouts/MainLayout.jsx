fa0744c0c4ff2ef70cb9396b400956c300bfa9dd
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-10-04 18:45:49 +0700

    mainlayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import Home from '../pages/Home'
+import React from "react";
+import Home from "../pages/Home";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <Home />
-  )
-}
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+    // <Home />
+  );
+};
 
-export default MainLayout
+export default MainLayout;
