f0b2907fcb266e0bb0f1e6dda3ce891927104378
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-10-04 18:44:21 +0700

    update link

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -11,10 +11,30 @@ const Category = () => {
         </p>
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-4 md:gap-5 lg:gap-8 text-center">
-        <a href="/product/electronic" className="h-32 rounded-lg bg-[#FFF3E3] flex items-center justify-center font-bold text-lg text-base-100 uppercase">Electronics</a>
-        <a href="" className="h-32 rounded-lg bg-[#FFF3E3] flex items-center justify-center font-bold text-lg text-base-100 uppercase">Jewelry</a>
-        <a href="" className="h-32 rounded-lg bg-[#FFF3E3] flex items-center justify-center font-bold text-lg text-base-100 uppercase">Men's Clothing</a>
-        <a href="" className="h-32 rounded-lg bg-[#FFF3E3] flex items-center justify-center font-bold text-lg text-base-100 uppercase">Women's Clothing</a>
+        <a
+          href="/products/category/electronics"
+          className="h-32 rounded-lg bg-[#FFF3E3] flex items-center justify-center font-bold text-lg text-base-100 uppercase"
+        >
+          Electronics
+        </a>
+        <a
+          href="/products/category/jewelery"
+          className="h-32 rounded-lg bg-[#FFF3E3] flex items-center justify-center font-bold text-lg text-base-100 uppercase"
+        >
+          Jewelry
+        </a>
+        <a
+          href="/products/category/men's clothing"
+          className="h-32 rounded-lg bg-[#FFF3E3] flex items-center justify-center font-bold text-lg text-base-100 uppercase"
+        >
+          Men's Clothing
+        </a>
+        <a
+          href="/products/category/women's clothing"
+          className="h-32 rounded-lg bg-[#FFF3E3] flex items-center justify-center font-bold text-lg text-base-100 uppercase"
+        >
+          Women's Clothing
+        </a>
       </div>
     </div>
   );
