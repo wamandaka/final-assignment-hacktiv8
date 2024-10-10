fffd60efe0ead235ffb17e6219f4accd1c3e33d4
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-10-10 16:29:21 +0700

    cart reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../features/product/productSlice";
 import authReducer from "../features/auth/authSlice";
+import cartReducer from "../features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
     auth: authReducer,
+    cart: cartReducer,
   },
 });
