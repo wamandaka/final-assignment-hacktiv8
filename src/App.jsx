9959adb845ad4a57e73ba461427c724c25b7e79e
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-10-11 10:56:43 +0700

    pindahin path router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { RouterProvider } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import router from "./router";
+import router from "./router/router";
 
 function App() {
   return (
